3ebcc377bf6386d08e6eb968ad9026405391512d
DMasudd <DMasudd@users.noreply.github.com>
2018-05-31 23:10:47 -0300

    Mod Pasajero
    Agregue atributos que faltaban

--- src/app/Main.java
+++ src/app/Main.java
@@ -5,12 +5,15 @@ import java.util.Scanner;
 import clases.Administrador;
 import clases.Cliente;
 import clases.Loguin;
+import clases.Pasajero;
 
 public class Main {
 
 	public static void main(String[] args) {
          Menu menu=new Menu();
          menu.Switch();
+		
+
 	}
 
 }
--- src/clases/Pasajero.java
+++ src/clases/Pasajero.java
@@ -1,5 +1,7 @@
 package clases;
 
+import java.util.ArrayList;
+
 public class Pasajero {
 
 	private String nombre;
@@ -7,6 +9,8 @@ public class Pasajero {
     private String DNI;
     private String direccion;
     private String telefono;
+    private boolean listNegra;
+    private ArrayList<Registro> listaRegistro;
     
     public Pasajero(String nomb,String apell,String dni,String direc,String tel)
     {
@@ -15,6 +19,8 @@ public class Pasajero {
     	DNI=dni;
     	direccion=direc;
     	telefono=tel;
+    	listaRegistro = new ArrayList<Registro>();
+    	listNegra = false;
     }
 
 	public String getNombre() {
@@ -39,6 +45,16 @@ public class Pasajero {
 
 	public void setDNI(String dNI) {
 		DNI = dNI;
+	}
+	
+	public void setListNegra(boolean a) {
+		listNegra = a;
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "Nombre: "+nombre+" \nApellido: "+apellido+"\nDNI: "+DNI+"\nDireccion: "+direccion+"\nTelefono: "+telefono;
 	}
     
 }
