9bea925165d732f37cefeb3becb235babd46e84c
DMasudd <DMasudd@users.noreply.github.com>
2018-05-31 23:21:08 -0300

    Pull pasajero.class
    Arregle la clase pasajero, habia quedado desconfigurada

--- src/clases/Pasajero.java
+++ src/clases/Pasajero.java
@@ -11,6 +11,7 @@ public class Pasajero {
     private String telefono;
     private boolean listNegra;
     private ArrayList<Registro> listaRegistro;
+
     
     public Pasajero(String nomb,String apell,String dni,String direc,String tel)
     {
@@ -21,6 +22,7 @@ public class Pasajero {
     	telefono=tel;
     	listaRegistro = new ArrayList<Registro>();
     	listNegra = false;
+
     }
 
 	public String getNombre() {
@@ -54,8 +56,67 @@ public class Pasajero {
 	@Override
 	public String toString() {
 		// TODO Auto-generated method stub
-		return "Nombre: "+nombre+" \nApellido: "+apellido+"\nDNI: "+DNI+"\nDireccion: "+direccion+"\nTelefono: "+telefono;
+		return "Nombre: "+nombre+" \nApellido: "+apellido+"\nDNI: "+DNI+"\nDireccion: "+direccion+"\nTelefono: "+telefono+"\nLista Negra: "+listNegra;
 	}
     
+	public boolean getListaNegra()
+	{
+		return listNegra;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((DNI == null) ? 0 : DNI.hashCode());
+		result = prime * result + ((apellido == null) ? 0 : apellido.hashCode());
+		result = prime * result + ((direccion == null) ? 0 : direccion.hashCode());
+		result = prime * result + (listNegra ? 1231 : 1237);
+		result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+		result = prime * result + ((telefono == null) ? 0 : telefono.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pasajero other = (Pasajero) obj;
+		if (DNI == null) {
+			if (other.DNI != null)
+				return false;
+		} else if (!DNI.equals(other.DNI))
+			return false;
+		if (apellido == null) {
+			if (other.apellido != null)
+				return false;
+		} else if (!apellido.equals(other.apellido))
+			return false;
+		if (direccion == null) {
+			if (other.direccion != null)
+				return false;
+		} else if (!direccion.equals(other.direccion))
+			return false;
+		if (listNegra != other.listNegra)
+			return false;
+		if (nombre == null) {
+			if (other.nombre != null)
+				return false;
+		} else if (!nombre.equals(other.nombre))
+			return false;
+		if (telefono == null) {
+			if (other.telefono != null)
+				return false;
+		} else if (!telefono.equals(other.telefono))
+			return false;
+		return true;
+	}
+
+
+	
 }
 
